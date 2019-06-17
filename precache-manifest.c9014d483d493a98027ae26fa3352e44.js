c46a3cf94bcaf9512d0750d58b48f628d0ef7dae
Sudesh Sharma <Sudesh.Sharma@visertechnosys.local>
2019-06-17 15:39:20 +0530

    Updates

---
+++ precache-manifest.c9014d483d493a98027ae26fa3352e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f965020df84fc60b081783f6f5985ed6",
+    "url": "/githubtest/index.html"
+  },
+  {
+    "revision": "77586e83cbff65230457",
+    "url": "/githubtest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/githubtest/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "77586e83cbff65230457",
+    "url": "/githubtest/static/js/main.b03b7993.chunk.js"
+  },
+  {
+    "revision": "a547f722b93d7153b63d",
+    "url": "/githubtest/static/js/runtime~main.b8d73180.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubtest/static/media/logo.5d5d9eef.svg"
+  }
+]);
