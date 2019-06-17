c6789c4112270da81e4bd52cff669bb4c6e667c4
sudeshsharma <sudesh18sharma@gmail.com>
2019-06-17 18:26:46 +0530

    added three steve jobs images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import jobs from './Pics/jobs.jpg'
 import job2 from './Pics/job2.jpg'
+import job3 from './Pics/job3.jpg'
 
 class Images extends React.Component{
     render(){
         return(
             <div>
                 <img src={jobs} alt="steve jobs"/><br/><br/>
-                <img src={job2} alt="steve jobs"/>
+                <img src={job2} alt="steve jobs"/><br/><br/>
+                <img src={job3} alt="steve jobs"/><br/><br/>
             </div>
         )
     }
