36b65f433062f0931c68dd8a1d08b6fa901891c0
sudeshsharma <sudesh18sharma@gmail.com>
2019-06-17 18:23:36 +0530

    added two images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import jobs from './Pics/jobs.jpg'
+import job2 from './Pics/job2.jpg'
 
 class Images extends React.Component{
     render(){
         return(
             <div>
-                <img src={jobs} alt="steve jobs"/>
+                <img src={jobs} alt="steve jobs"/><br/><br/>
+                <img src={job2} alt="steve jobs"/>
             </div>
         )
     }
